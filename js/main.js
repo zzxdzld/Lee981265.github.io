d18c08b423795e0a4a04fdedbc55565a21648a85
Lee <Lee981265@gmail.com>
2019-01-14 18:27:03 +0800

    Echarts 属性

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 require([], function (){
+    var e = "%c";
 
+    var n = "color:red;text-shadow:5px 5px 2px #fff, 5px 5px 2px #373E40, 5px 5px 5px #A2B4BA, 5px 5px 10px #82ABBA;font-weight:bolder;font-size:55px"
+
+    var r = "color:#495A80;text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 1px 0 #bbb;font-size:20px";
+
+    console.info(e + "哈哈", n);
+
+    console.info(e + "你知道,你在干什么?:-)\r\n", r);
+
+    
     var isMobileInit = false;
     var loadMobile = function(){
         require([yiliaConfig.rootUrl + 'js/mobile.js'], function(mobile){
