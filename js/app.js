1b227023b1594092f4a474c0a38879753c042fcb
Lee <Lee981265@gmail.com>
2019-01-18 11:57:29 +0800

    随笔

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@
  *  hexo-theme-snippet: app.js v1.0.0
  * ======================================================================== */
 window.onload = function() {
+  var e = "%c";
+
+  var n = "color:red;text-shadow:5px 5px 2px #fff, 5px 5px 2px #373E40, 5px 5px 5px #A2B4BA, 5px 5px 10px #82ABBA;font-weight:bolder;font-size:55px"
+
+  var r = "color:#495A80;text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 1px 0 #bbb;font-size:20px";
+
+  console.info(e + "你好，朋友，欢迎你的来访！", n);
+
+  console.info(e + "前端交流群：635678224 期待你的加入。:-)\r\n", r);
+
+
     var $body = document.body,
         $mnav = document.getElementById("mnav"), //获取导航三角图标
         $mainMenu = document.getElementById("main-menu"), //手机导航
